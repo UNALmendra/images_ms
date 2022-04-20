328c8faf802946a9c70a8c4ce2cb02d6cc761d42
jusruizsa <sruizs.jr@gmail.com>
2022-04-20 00:01:34 -0500

    first commit

---
+++ app/app.py
@@ -0,0 +1,29 @@
+from crypt import methods
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from pdf2img import pdf2img
+from img2pdf import img2pdf
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/', methods=['GET'])
+def hola():
+    return jsonify({"message": "Holaaaaa!!"})
+
+class ConvertPDF(Resource):
+    def post(self):
+        return 'Archivo convertido exitosamente. Imagen guardada en ' + pdf2img(), 201
+
+class ConvertImg(Resource):
+    def post(self):
+        return 'Archivo convertido exitosamente. pdf guardado en ' + img2pdf(), 201
+
+
+
+api.add_resource(ConvertPDF, '/pdf2img')
+api.add_resource(ConvertImg, '/img2pdf')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=3000, debug=True)
---
+++ app/pdf2img.py
@@ -0,0 +1,33 @@
+from pdf2image import convert_from_path
+import os
+
+
+from pdf2image.exceptions import(
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+
+
+# path_pictures = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Pictures')
+# path_pdf2img = path_pictures + "\\pdf2images\\"
+
+
+path_pdf2img = 'UNpdf/pdf2img/'
+
+file = '../test.pdf'
+
+def pdf2img():
+
+    if not os.path.exists(path_pdf2img):
+        os.makedirs(path_pdf2img)
+
+    
+    pages = convert_from_path(file, 500)
+    counter = 1
+    for page in pages:
+        myfile = path_pdf2img + 'output' + str(counter) + '.jpg'
+        counter = counter + 1
+        page.save(myfile, "JPEG")
+
+    return path_pdf2img
